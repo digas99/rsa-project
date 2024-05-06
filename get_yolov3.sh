d2ae9ed83247dab8106a038598fe23cd682f61b1
Diogo Correia <diogo.correia99@ua.pt>
2024-05-06 22:36:22 +0100

    fixed yolo sources

--- get_yolov3.sh
+++ get_yolov3.sh
@@ -1,3 +1,3 @@
 wget https://pjreddie.com/media/files/yolov3.weights
-wget https://github.com/pjreddie/darknet/blob/master/cfg/yolov3.cfg
-wget https://github.com/pjreddie/darknet/blob/master/data/coco.names
+wget https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3.cfg
+wget https://raw.githubusercontent.com/pjreddie/darknet/master/data/coco.names
