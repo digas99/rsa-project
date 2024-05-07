60bc49cc31531ebe1bb828fe26111d71e17a37d0
Diogo Correia <diogo.correia99@ua.pt>
2024-05-07 13:09:25 +0100

    updated detection class

--- src/app.py
+++ src/app.py
@@ -1,16 +1,41 @@
+import argparse
+import cv2
 import os
-
-# Set YOLOv8 to quiet mode
-os.environ['YOLO_VERBOSE'] = 'False'
 
 from detection import Detector
 
-def callback(people):
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def handle_image(people, frame):
 	print(f"People detected: {people}")
 
+	output_dir = os.path.join(BASE_DIR, "../output")
+	if not os.path.exists(output_dir):
+		os.makedirs(output_dir)
+	
+	num_files = len(os.listdir(output_dir))
+	cv2.imwrite(f"{output_dir}/output{num_files}.jpg", frame)
+
+def handle_stream(people, frame):
+	print(f"People detected: {people}")
+
+
 def main():
+	# handle command line arguments
+	ap = argparse.ArgumentParser()
+	ap.add_argument('-i', '--image', required=False,
+					help = 'path to input image')
+	
+	args = ap.parse_args()
+	
 	detector = Detector()
-	detector.stream(callback, show=True)
+	
+	if args.image:
+		image = cv2.imread(args.image)
+		detector.detect(handle_image, image, object="person")
+	else:
+		detector.stream(handle_stream, object="person")
 
 if __name__ == "__main__":
 	main()
--- src/detection.py
+++ src/detection.py
@@ -14,11 +14,40 @@ class Detector:
               "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
               "teddy bear", "hair drier", "toothbrush"
               ]
-		self.model = YOLO("yolov8/yolov8n.pt", verbose=False)
-		self.people = 0
+		self.model = YOLO("yolov8/yolov8n.pt")
 
-		
-	def stream(self, callback, show=True):
+	def detect(self, callback, image, object, stream=False, show=True):
+		results = self.model(image, stream, verbose=False)
+		counter = 0
+		for result in results:
+			boxes = result.boxes
+			for box in boxes:
+				cls = int(box.cls[0])
+				if self.classNames[cls] == object:
+					counter += 1
+				
+				if self.classNames[cls] == object:
+					x, y, w, h = box.xyxy[0]
+					x, y, w, h = int(x), int(y), int(w), int(h)
+					cv2.rectangle(image, (x, y), (w, h), (0, 255, 0), 2)
+
+					cv2.putText(image, self.classNames[cls], (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+
+		if callback:
+			callback(counter, image)
+
+		if show:
+			cv2.imshow("Frame", image)
+			
+			if stream:
+				return
+			
+			cv2.waitKey(0)
+			cv2.destroyAllWindows()
+
+		return counter
+
+	def stream(self, callback, object="person", show=True):
 		cap = cv2.VideoCapture(0)
 		cap.set(3, 640)
 		cap.set(4, 480)
@@ -28,28 +57,11 @@ class Detector:
 			if not ret:
 				break
 
-			results = self.model(frame, stream=True)
-			self.people = 0
-			for result in results:
-				boxes = result.boxes
-				for box in boxes:
-					cls = int(box.cls[0])
-					if self.classNames[cls] == "person":
-						self.people += 1
-					
-					if show and self.classNames[cls] == "person":
-						x, y, w, h = box.xyxy[0]
-						x, y, w, h = int(x), int(y), int(w), int(h)
-						cv2.rectangle(frame, (x, y), (w, h), (0, 255, 0), 2)
+			self.detect(callback, frame, object, stream=True, show=show)
 
-						cv2.putText(frame, self.classNames[cls], (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
-
-			callback(self.people)
-
-			if show:
-				cv2.imshow("Frame", frame)
-				if cv2.waitKey(1) & 0xFF == ord("q"):
-					break
+			if cv2.waitKey(1) & 0xFF == ord("q"):
+				cv2.destroyAllWindows()
+				break
 
 		cap.release()
 		cv2.destroyAllWindows()
